1ffba41fb69c7520d3a181fff04623972bdbd46d
Martin Bram Moreinis <martin.moreinis@schoolsofwestfield.org>
2025-02-12 15:55:36 +0000

    started

--- script.js
+++ script.js
@@ -1,83 +1,11 @@
-/* Nim Trainer by Mr. M.
- * based on this flowchart:
- * https://lucid.app/lucidchart/2018baaf-4c26-4a76-a0d5-93c97f444425/view
- */
-
-/** 
- * main  
- * Handles new Nim games with gametype choice simple or trainer and a play again option. 
- * @param none 
- * @return none
- */
-
-// global variables
-var trainer = false;
-var count = 0;
-
-/* Main */
 function main(){
-    let again = false;
-    trainer = confirm("Would you like to use trainer mode?")
-    playNim();
-    again = confirm("Play again?");
-    if (again == true) main();
-}
-
-/** 
- * playNim 
- * plays a game with user first and computer second. Winner declared in an alert box. 
- * @param none 
- * @return none
- */
-
-/* playNim */
-function playNim(){
-    count = 0;
+    var count = 0;
     while (count < 21){
-        userTurn();
-        if (count > 20) alert("You lose!");
-        else{
-            cpuTurn();
-            if (count > 20) alert("You win!");
+        var turn = prompt("Next count?");
+        if (turn == 1 || turn == 2 || turn == 3){
+            
         }
+
     }
+
 }
-
-/** 
- * userTurn  
- * User enters a turn. Validation against cheating handled by recursion.
- * @param none 
- * @return none
- */
-function userTurn(){
-    let turn = prompt("Pick a number, 1-3.");
-    turn = parseInt(turn);
-    if (turn < 1 || turn > 3){
-        alert("Your input is invalid!")
-        userTurn();
-    }
-    else{
-        count += turn;
-        alert("Count is now " + count + ".");
-    }
-}
-
-/** 
- * cpuTurn 
- * Generate computer's turn without losing on purpose.  Different turns if trainer or simple.  
- * @param none 
- * @return none
- */
-function cpuTurn(){
-    let turn = 0;
-    if (count == 17) turn = 3;
-    else if (count == 18) turn = 2;
-    else if (count > 18) turn = 1;
-    else if (trainer == true) turn = 4 - count % 4;
-    else turn = Math.floor(Math.random()*3)+1;
-    count += turn;
-    alert("I counted " + turn + ", count is now " + count + ".");
-}
-
-
-
