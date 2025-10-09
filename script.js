b24cbebef4f45c8d653efb3ddf910b71071330d4
Martin Bram Moreinis <martin.moreinis@schoolsofwestfield.org>
2025-10-09 13:54:56 +0000

    Add E block, 10/9

--- script.js
+++ script.js
@@ -1,11 +1,54 @@
+/* Nim Trainer by [your name]
+ * based on this flowchart:
+ * https://lucid.app/lucidchart/2018baaf-4c26-4a76-a0d5-93c97f444425/view
+ */
+
+/* Global Variables */
+var trainer = false;
+var count = 0;
+
+/** 
+ * main  
+ * Handles new Nim games with gametype choice simple or trainer and a play again option. 
+ * @param none 
+ * @return none
+ */
+/* Main */
 function main(){
-    var count = 0;
-    while (count < 21){
-        var turn = prompt("Next count?");
-        if (turn == 1 || turn == 2 || turn == 3){
-            
-        }
-
+    let again = true;
+    while (again == true) {
+        trainer = confirm("Trainer mode?");
+        playNim();
+        again = confirm("Play again?");
     }
+}
 
+/** 
+ * playNim 
+ * plays a game with user first and computer second. Winner declared in an alert box. 
+ * @param none 
+ * @return none
+ */
+function playNim(){
+    alert("Hello");
+}
+
+/** 
+ * userTurn  
+ * User enters a turn. Validation against cheating handled by recursion.
+ * @param none 
+ * @return none
+ */
+function userTurn(){
+    count+=3;
+}
+
+/** 
+ * cpuTurn 
+ * Generate computer's turn without losing on purpose.  Different turns if trainer or simple.  
+ * @param none 
+ * @return none
+ */
+function cpuTurn(){
+    count+=3;
 }
